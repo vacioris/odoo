ee23db9dd8f4fab7b0165961095ee8d981a1dfc5
Vacioris <96044195+vacioris@users.noreply.github.com>
2025-05-08 15:17:08 +0300

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -14,7 +14,7 @@
 # ./odoo-install
 ################################################################################
 
-OE_USER="odoo15"
+OE_USER="odoo"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 # The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
